8008f7810743c170ea5c408ee482aea1f227b1e2
vladpro-mina <83288388+vladpro-mina@users.noreply.github.com>
2025-10-22 14:27:02 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,7 @@ GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .n
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
-wget https://raw.githubusercontent.com/RaiderT/VerusCliMining/main/config.json -O ~/ccminer/config.json
-wget https://raw.githubusercontent.com/RaiderT/VerusCliMining/main/config_luckpool.json -O ~/ccminer/config_luckpool.json
-wget https://raw.githubusercontent.com/RaiderT/VerusCliMining/main/config_zerg_party.json -O ~/ccminer/config_zerg_party.json
+wget https://raw.githubusercontent.com/vladpro-mina/VerusCliMining/main/config.json -O ~/ccminer/config.json
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
@@ -24,18 +22,6 @@ cat << EOF > ~/ccminer/start.sh
 ~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
 chmod +x start.sh
-
-cat << EOF > ~/ccminer/start2.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_luckpool.json
-EOF
-chmod +x start2.sh
-
-cat << EOF > ~/ccminer/start3.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_zerg_party.json
-EOF
-chmod +x start3.sh
 
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
